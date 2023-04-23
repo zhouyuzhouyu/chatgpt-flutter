7d9a229236fffc266914d4e7c0fef90b472f2f31
zhou yu <524112470@qq.com>
2023-04-23 10:06:29 +0800

    modify ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,102 @@
 import 'package:chatgpt/env/env.dart';
 import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
 import 'package:dart_openai/openai.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatPage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  final textController = TextEditingController();
-  String responseText = '';
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
 
-  Future<String> generateText(String prompt) async {
+class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _messages = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ChatGPT'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length * 2,
+              itemBuilder: (context, index) {
+                if (index.isEven) {
+                  final message = _messages[index ~/ 2];
+                  return Container(
+                    padding: EdgeInsets.all(8),
+                    child: SelectableText(
+                      message,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  );
+                } else {
+                  return Divider(
+                    color: Colors.grey,
+                    thickness: 1,
+                  );
+                }
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      hintText: 'Type your message...',
+                    ),
+                  ),
+                ),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: _sendMessage,
+                  child: Text('Send'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _sendMessage() {
+    final prompt = _controller.text;
+    if (prompt.isNotEmpty) {
+      _controller.clear();
+      _messages.add('USER: $prompt');
+      setState(() {});
+      chatGPT(prompt).then((response) {
+        _messages.add('ASSISTANT: $response');
+        setState(() {});
+      });
+    }
+  }
+
+  Future<String> chatGPT(String prompt) async {
     OpenAI.apiKey = Env.apiKey;
     OpenAIChatCompletionModel chatCompletion =
         await OpenAI.instance.chat.create(
@@ -29,63 +108,20 @@ class _MyAppState extends State<MyApp> {
         ),
       ],
     );
-    // print(chatCompletion.toMap());
     return chatCompletion.choices.first.message.content;
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'OpenAI Chat/gpt3.5-turbo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('OpenAI Chat/gpt3.5-turbo'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'AI: ' + responseText,
-                      textAlign: TextAlign.start,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      controller: textController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'You:',
-                      ),
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    String prompt = textController.text;
-                    String response = await generateText(prompt);
-                    setState(() {
-                      responseText = response;
-                    });
-                    textController.clear();
-                  },
-                  child: Text('Send'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
+
+// Future<String> chatGPT(String prompt) async {
+//   OpenAI.apiKey = Env.apiKey;
+//   OpenAIChatCompletionModel chatCompletion = await OpenAI.instance.chat.create(
+//     model: "gpt-3.5-turbo",
+//     messages: [
+//       OpenAIChatCompletionChoiceMessageModel(
+//         content: prompt,
+//         role: OpenAIChatMessageRole.user,
+//       ),
+//     ],
+//   );
+//   return chatCompletion.choices.first.message.content;
+// }
